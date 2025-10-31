d376bd399776a0b7e2646063a5bdeb67fde90af5
KTSHACX <mine3krish@gmail.com>
2025-11-01 03:28:04 +0530

    SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        destination: 'https://zebriustechnologies.com/$1',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
